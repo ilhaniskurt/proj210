67ad1326711a21b47b88bb113ccab050beb280ee
İlhan Yavuz İskurt <ilhaniskurt@Ilhans-MacBook-Air.local>
2023-05-29 22:07:15 +0300

    update ilo analyzer (incomplete)

--- analyzers/ilo.py
+++ analyzers/ilo.py
@@ -1,11 +1,36 @@
 # Scraper for ilo.org
-# Author: Ilhan Yavuz Iskurt
+# Author: Ilhan Yavuz Iskurt, Alp Yener
 
 # External imports
+from pathlib import Path
+import pandas as pd
+# import numpy as np
+# import seaborn as sns
+# import matplotlib.pyplot as plt
 
 # Local imports
 from utils.config import config
 
 
 def analyze():
-    pass
+    # Read csv
+    datapath = Path(config.DATA_DIR)
+    file = list(datapath.glob(config.ILO_DATA + "*"))
+    df = pd.read_csv(file[0])
+
+    label_column = 'indicator.label'
+
+    # Get unique labels from the label column
+    labels = df[label_column].unique()
+
+    # Create separate DataFrames for each label
+    sep_dfs = {
+        label: df[df[label_column] == label] for label in labels}
+
+    # Chart 1
+    df1 = sep_dfs[
+        "Inflow of working age foreign citizens by sex and country of citizenship (thousands)"]
+    print(df1.iloc[1:6].columns)
+    print(df1["time"].head())
+    # for k in sep_dfs.keys():
+    #     print(k)
--- run.py
+++ run.py
@@ -4,13 +4,20 @@
 # Local imports
 from utils.config import config
 from utils.dl import data_exists
-from scrapers import ilo
+from scrapers.ilo import scrape as ilo_scrape
+from analyzers.ilo import analyze as ilo_analyze
 
 # International Labour Organization
-print("ILO process started!")
+print("--- ILO process started! ---")
 if data_exists(config.ILO_DATA):
     print("[ILO] Data cached found! Skipping scrapping.")
 else:
     print("[ILO] Data scrapping..")
-    ilo.scrape()
+    ilo_scrape()
     print("[ILO] Data scrapped!")
+
+print("[ILO] Data analysis..")
+ilo_analyze()
+print("[ILO] Data analysis completed!")
+
+print("--- ILO process ended ---")
