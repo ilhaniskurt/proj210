79b7907e2ca123da4fd3179c0e7b00503053e8d1
İlhan Yavuz İskurt <ilhaniskurt@Ilhans-MacBook-Air.local>
2023-06-09 22:02:07 +0300

    update ilo analyzer (janset)

--- analyzers/ilo.py
+++ analyzers/ilo.py
@@ -24,59 +24,59 @@ def graph1(df: pd.DataFrame):
     fig, ax = plt.subplots(figsize=(10, 6))
     for label, group in df_top.groupby("classif1.label"):
         label = label.replace("Country of citizenship: ", "")
+
+        # Sort the group by time values
+        group = group.sort_values("time")
+
         ax.plot(group["time"], group["obs_value"], label=label)
 
     # Set labels and title
     ax.set_xlabel('Time')
-    ax.set_ylabel('Migration')
+    ax.set_ylabel('Migration by thousand')
     ax.set_title('Migration over Time')
 
     # Add a legend
     ax.legend()
 
     # Save the plot as a file (e.g., PNG format)
-    plt.savefig(config.GRAPH_DIR / "test.png")
+    plt.savefig(config.GRAPH_DIR / "graph1.png")
 
-
-
-# Now it only plots for foreign
 
 def graph2(df: pd.DataFrame):
     df = df.drop("indicator.label", axis=1)
-    
+
     df = df.drop(["ref_area.label", "source.label", "obs_status.label",
                  "note_classif.label", "note_indicator.label", "note_source.label"], axis=1)
-    
-    total_df: pd.DataFrame = df[df["classif2.label"] != "Place of birth: Total"]
-    total_df = total_df[total_df["classif2.label"] != "Place of birth: Status unknown"]
-    total_df = total_df[total_df["classif1.label"] == "Education (Aggregate levels): Total"]
+
+    total_df: pd.DataFrame = df[df["classif2.label"]
+                                != "Place of birth: Total"]
+    total_df = total_df[total_df["classif2.label"]
+                        != "Place of birth: Status unknown"]
+    total_df = total_df[total_df["classif1.label"]
+                        == "Education (Aggregate levels): Total"]
     total_df = total_df[total_df["sex.label"] == "Sex: Total"]
-    
+
     total_df = total_df.drop(["classif1.label", "sex.label"], axis=1)
-    
-    df_native = total_df[total_df['classif2.label'] == 'Place of birth: Native-born']
-    df_foreign = total_df[total_df['classif2.label'] == 'Place of birth: Foreign-born']
-    
+
+    df_native = total_df[total_df['classif2.label']
+                         == 'Place of birth: Native-born']
+    df_foreign = total_df[total_df['classif2.label']
+                          == 'Place of birth: Foreign-born']
+
     df_sorted = df_foreign.sort_values('time')
 
     # Reset the index of the sorted DataFrame
     df_sorted = df_sorted.reset_index(drop=True)
 
-    
     plt.plot(df_sorted['time'], df_sorted['obs_value'], label='Foreign')
-    
+
     plt.xlabel('Time')
     plt.ylabel('Value')
     plt.title('Line Graph')
     plt.legend()
 
     # Show the plot
-    plt.show()
-    
-    
-
-
-
+    plt.savefig(config.GRAPH_DIR / "graph2.png")
 
 
 def analyze():
@@ -87,12 +87,10 @@ def analyze():
 
     label_column = 'indicator.label'
 
-    '''
-    # Drop irrelevant columns from DataFrame 
-    df = df.drop(["ref_area.label", "source.label", "classif2.label", "obs_status.label",
-                 "note_classif.label", "note_indicator.label", "note_source.label"], axis=1)
-    '''
-    
+    # Drop irrelevant columns from DataFrame
+    # df = df.drop(["ref_area.label", "source.label", "classif2.label", "obs_status.label",
+    # "note_classif.label", "note_indicator.label", "note_source.label"], axis=1)
+
     # Get unique labels from the label column
     labels = df[label_column].unique()
 
@@ -101,8 +99,9 @@ def analyze():
         label: df[df[label_column] == label] for label in labels}
 
     # Chart 1
-    #graph1(sep_dfs[
-    #    "Inflow of working age foreign citizens by sex and country of citizenship (thousands)"])
-    
-    #Chart 2
-    graph2(sep_dfs["Persons outside the labour force by sex, education and place of birth (in thousands)"])
+    graph1(sep_dfs[
+        "Inflow of working age foreign citizens by sex and country of citizenship (thousands)"])
+
+    # Chart 2
+    graph2(
+        sep_dfs["Persons outside the labour force by sex, education and place of birth (in thousands)"])
